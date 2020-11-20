daa52cf62d9c339bbdb9dc25915cd212a6435951
17hoehbr <17hoehbr@gmail.com>
2020-11-20 02:13:04 -0500

    enabled beta by default, optimized + cleaned code

--- index.js
+++ index.js
@@ -1,96 +1,87 @@
 const { app, BrowserWindow, nativeTheme, Menu } = require('electron');
-const fs = require('fs');
-const path = require('path');
-const windowStateKeeper = require('electron-window-state');
+
+// Check for updates
 const { autoUpdater } = require("electron-updater");
-const log = require("electron-log");
-const Store = require("electron-store");
-
-//-------------------------------------------------------------------
-// Logging
-//
-// THIS SECTION IS NOT REQUIRED
-//
-// This logging setup is not required for auto-updates to work,
-// but it sure makes debugging easier :)
-//-------------------------------------------------------------------
-autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-log.info('App starting...');
-
-// Checks for updates
 app.on('ready', function() {
   autoUpdater.checkForUpdatesAndNotify();
 });
 
+// Logging
+const log = require("electron-log");
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+
 // electron-store
+const Store = require("electron-store");
 const storeSchema = {
   beta: {
     type: 'boolean',
-    default: false
+    default: true
   },
   darkmode: {
     type: 'boolean',
     default: null
   }
 };
-
 const store = new Store(storeSchema);
 
 // Custom menu
-const menuSchema = [
-  {
-    label: "Settings",
-    submenu: [{
-    click: () => {
-      store.set("beta", !store.get("beta")); // change beta setting on click
-      app.relaunch();
-      app.exit(); // restarts the app
+const menuSchema = [{
+  label: "Settings",
+  submenu: [{
+      click: () => {
+        store.set("beta", !store.get("beta")); // change beta setting on click
+        app.relaunch();
+        app.exit(); // restarts the app
+      },
+      type: "checkbox",
+      label: "Use beta version",
+      enabled: true,
+      checked: store.get("beta")
     },
-    type: "checkbox",
-    label: "Use beta version",
-    enabled: true,
-    checked: store.get("beta")
-  },
-  {
-    label: "Dark mode",
-    type: "checkbox",
-    enabled: true,
-    checked: store.get("darkmode") === null ? nativeTheme.shouldUseDarkColors : store.get("darkmode"),
-    click: () => {
-      if (store.get("darkmode") === null) {
-        store.set("darkmode", !nativeTheme.shouldUseDarkColors)
-      } else store.set("darkmode", !store.get("darkmode"))
-      nativeTheme.themeSource = store.get("darkmode") ? "dark" : "light"
+    {
+      label: "Dark mode",
+      type: "checkbox",
+      enabled: true,
+      checked: store.get("darkmode") === null ? nativeTheme.shouldUseDarkColors : store.get("darkmode"),
+      click: () => {
+        if (store.get("darkmode") === null) {
+          store.set("darkmode", !nativeTheme.shouldUseDarkColors)
+        } else store.set("darkmode", !store.get("darkmode"))
+        nativeTheme.themeSource = store.get("darkmode") ? "dark" : "light"
+      }
     }
-  }
-]
+  ]
 }];
 const menu = Menu.buildFromTemplate(menuSchema);
 Menu.setApplicationMenu(menu);
+
 // Create window
 function createWindow() {
+  const windowStateKeeper = require('electron-window-state');
   // Window state
   let mainWindowState = windowStateKeeper({
-      defaultWidth: 1000,
-      defaultHeight: 800
+    defaultWidth: 1000,
+    defaultHeight: 800
   });
   // Create the browser window.
+  const path = require('path');
   const win = new BrowserWindow({
-      icon: path.join(__dirname, 'icon.png'),
-      'x': mainWindowState.x,
-      'y': mainWindowState.y,
-      'width': mainWindowState.width,
-      'height': mainWindowState.height,
-      minWidth: 350,
-      minHeight: 100,
-      // Enables DRM
-      webPreferences: {
-          plugins: true,
-          nodeIntegration: false,
-          contextIsolation: true,
-          sandbox: true
-      }
+    icon: path.join(__dirname, 'icon.png'),
+    'x': mainWindowState.x,
+    'y': mainWindowState.y,
+    'width': mainWindowState.width,
+    'height': mainWindowState.height,
+    minWidth: 350,
+    minHeight: 100,
+    // Enables DRM
+    webPreferences: {
+      plugins: true,
+      nodeIntegration: false,
+      contextIsolation: true,
+      sandbox: true
+    }
   });
   // Let us register listeners on the window, so we can update the state
   // automatically (the listeners will be removed when the window is closed)
@@ -106,28 +97,32 @@ function createWindow() {
   switch (store.get("darkmode")) {
     case null:
       nativeTheme.themeSource = "system";
+      if (nativeTheme.shouldUseDarkColors) win.setBackgroundColor('#1f1f1f');
       break;
     case true:
       nativeTheme.themeSource = "dark";
+      win.setBackgroundColor('#1f1f1f');
       break;
     default:
       nativeTheme.themeSource = "light";
       break;
   }
-
   // injects css from styles.css
+  const fs = require('fs');
   win.webContents.on('did-finish-load', function() {
-      fs.readFile(__dirname + '/styles.css', "utf-8", function(error, data) {
-          if (!error) {
-              var formatedData = data.replace(/\s{2,10}/g, ' ').trim();
-              win.webContents.insertCSS(formatedData);
-          }
-      });
-  });
-  // nukes electron when close button clicked
-  win.on('close', () => {
-      app.exit();
+    fs.readFile(__dirname + '/styles.css', "utf-8", function(error, data) {
+      if (!error) {
+        var formatedData = data.replace(/\s{2,10}/g, ' ').trim();
+        win.webContents.insertCSS(formatedData);
+      }
+    });
   });
 }
+
 // enables DRM and opens window
 app.on('widevine-ready', createWindow);
+
+// nukes electron when close button clicked
+app.on('close', () => {
+  app.exit();
+});
