032e2762c2a7dbf756d411eb7c8fb2d7c901031e
17hoehbr <17hoehbr@gmail.com>
2020-11-22 00:45:40 -0500

    uses musi.sh unofficial client

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, nativeTheme, Menu } = require('electron');
+const { app, BrowserWindow } = require('electron');
 require('v8-compile-cache');
 
 // Check for updates
@@ -13,55 +13,6 @@ autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = 'info';
 log.info('App starting...');
 
-// electron-store
-const Store = require("electron-store");
-const storeSchema = {
-  beta: {
-    type: 'boolean',
-    default: true
-  },
-  darkmode: {
-    type: 'boolean',
-    default: null
-  }
-};
-const store = new Store(storeSchema);
-
-// Custom menu
-const menuSchema = [{
-  label: "Settings",
-  submenu: [{
-      click: () => {
-        store.set("beta", !store.get("beta")); // change beta setting on click
-        app.relaunch();
-        app.exit(); // restarts the app
-      },
-      type: "checkbox",
-      label: "Use beta version",
-      enabled: true,
-      checked: store.get("beta")
-    },
-    {
-      label: "Dark mode",
-      type: "checkbox",
-      enabled: true,
-      checked: store.get("darkmode") === null ? nativeTheme.shouldUseDarkColors : store.get("darkmode"),
-      click: () => {
-        if (store.get("darkmode") === null) {
-          store.set("darkmode", !nativeTheme.shouldUseDarkColors)
-        } else store.set("darkmode", !store.get("darkmode"))
-        nativeTheme.themeSource = store.get("darkmode") ? "dark" : "light"
-      }
-    },
-    {
-      label: "Dev Tools",
-      role: "toggleDevTools"
-    }
-  ]
-}];
-const menu = Menu.buildFromTemplate(menuSchema);
-Menu.setApplicationMenu(menu);
-
 // Cache
 const express = require("express");
 const fileCacheMiddleware = require("express-asset-file-cache-middleware");
@@ -72,8 +23,7 @@ cache.get(
   "/assets/:asset_id",
   async (req, res, next) => {    
     res.locals.fetchUrl = [
-      `http://music.apple.com`,
-      'http://beta.music.apple.com'
+      'http://musi.sh'
     ];
     next();
   },
@@ -111,8 +61,6 @@ function createWindow() {
     // Enables DRM
     webPreferences: {
       plugins: true,
-      nodeIntegration: false,
-      contextIsolation: true,
       sandbox: true
     }
   });
@@ -126,31 +74,7 @@ function createWindow() {
   win.setAutoHideMenuBar(true);
   // load Apple Music based on the store value
   // use http instead of https to fix adam id bug
-  win.loadURL(store.get("beta") ? "http://beta.music.apple.com" : "http://music.apple.com");
-  // dark mode setting
-  switch (store.get("darkmode")) {
-    case null:
-      nativeTheme.themeSource = "system";
-      if (nativeTheme.shouldUseDarkColors) win.setBackgroundColor('#1f1f1f');
-      break;
-    case true:
-      nativeTheme.themeSource = "dark";
-      win.setBackgroundColor('#1f1f1f');
-      break;
-    default:
-      nativeTheme.themeSource = "light";
-      break;
-  }
-  // injects css from styles.css
-  const fs = require('fs');
-  win.webContents.on('did-finish-load', function() {
-    fs.readFile(__dirname + '/styles.css', "utf-8", function(error, data) {
-      if (!error) {
-        var formatedData = data.replace(/\s{2,10}/g, ' ').trim();
-        win.webContents.insertCSS(formatedData);
-      }
-    });
-  });
+  win.loadURL("http://musi.sh");
   // shows when ready
   win.once('ready-to-show', () => {
     win.show()
